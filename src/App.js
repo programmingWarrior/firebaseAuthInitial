8817b6cbe049629325a25d176167e8ebfa45c1cb
shawonahmed1 <mdshawon356@gmail.com>
2020-09-08 21:04:02 +0600

    Before starting with firebase this file is needed. This is the complete resourse of before starting firebase Auth

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import { Button } from 'react-bootstrap';
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from './firebase.config';
+
+firebase.initializeApp(firebaseConfig);
+
+function App() {
+  return (
+   <div>
+     
+     
+    </div>
+  );
+}
+
+export default App;
